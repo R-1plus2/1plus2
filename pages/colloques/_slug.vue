1c3fc29599a1847c2a3d57d979b93fd261ffb6e2
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-07-29 14:44:39 +0200

    Update _slug.vue

--- pages/colloques/_slug.vue
+++ pages/colloques/_slug.vue
@@ -18,16 +18,9 @@
               </div>
               <div class="accordeon__body">
                 <div class="accordeon__content">
-                  <div class="accordeon__info">
-                      <h5></h5>
-                      <small>DD/MM/YYYY</small>
-                      <p>
-                        Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn.
-                        Shagg athg gof'nn hupadgh R'lyeh, sll'ha kn'a kadishtu y-Tsathoggua cwgah'n ilyaa ee hafh'drn mg tharanak, throd kadishtu naflAzathoth f'r'luh gof'nn f'uln ron ilyaa n'gha.
-                        Shtunggli Cthulhunyth llll Shub-Niggurath hai gotha n'ghft geb kn'aor ebunma n'gha nglui, Tsathoggua lloignyth ronoth hupadgh kadishtu nw naflah grah'n nak'yarnak uln Tsathoggua y'hah, R'lyeh epor wgah'n sgn'wahl naflkn'a throd Yoggoth nw ftaghu n'ghft.
-                        Ebunma vulgtm vulgtlagln lw'nafh shogg ron, Nyarlathotep cvulgtm nnnTsathoggua stell'bsna R'lyeh s'uhn, nwor ya R'lyeh shugg.
-                        H'gof'nn r'luh kn'a Nyarlathotep sgn'wahl shuggnyth uh'e, Azathoth sgn'wahlor naflstell'bsna hlirghor Tsathoggua throd, geboth n'ghft cShub-Niggurath ph'Chaugnar Faugn Dagon.
-                      </p>
+                  <div class="accordeon__info" v-for="b in a.article">
+                      <small>{{ b.horaire }}</small>
+                      <p>{{ b.titre }}</p>
                   </div>
                 </div>
               </div>
