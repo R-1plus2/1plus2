6517bcff8d663e94cba13a2c8e1305c28ed5af2e
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-08-05 13:46:28 +0200

    Update index.vue

--- pages/actualites/index.vue
+++ pages/actualites/index.vue
@@ -65,7 +65,7 @@ export default {
            $(this).html(modif);
         });
         $('.category').each( function( ) {
-           var modif = $(this).html().after( document.createTextNode( "," ) );
+           var modif = $(this).html().after( $( "<p>,</p>" ) );
            $(this).html(modif);
         });
      },
