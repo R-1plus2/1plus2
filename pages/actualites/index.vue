3e9cc2ce41b2f9e8eb3cd3f3931e3fb3a40b096f
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-08-05 10:58:23 +0200

    Update index.vue

--- pages/actualites/index.vue
+++ pages/actualites/index.vue
@@ -43,7 +43,8 @@ export default {
         _path: `/actualites/${key.replace('.json', '').replace('./', '')}`
       }));
       return {
-        actualites
+        actualites,
+        grid: null
       };
     },
   mounted() {
