36cf9624a23f28dbc5a4937ce7dad2438fedff7c
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-08-06 16:19:07 +0200

    Create sidebar.vue

---
+++ components/ui/sidebar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="sidebar">
+      <div class="accordeon">
+          <div class="accordeon__head">
+              <div class="accordeon__titre">
+                  <h4>CATÉGORIES</h4>
+              </div>
+              <div class="accordeon__icon">
+                  <i class="fas fa-angle-down collapsible__icon"></i>
+              </div>
+          </div>
+          <div class="accordeon__body">
+              <div class="accordeon__content">
+                  <div class="accordeon__info" >
+                      <div class="horaires">{{ cat.title }}</div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import $ from 'jquery'
+export default {
+  async asyncData({ params }) {
+    let cat = await import('~/content/categorie/page/slug.json');
+    return cat;
+  },
+  mounted: () => { 
+  },
+  methods: {
+  }
+}
+</script>
