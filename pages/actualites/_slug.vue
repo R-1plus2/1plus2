c1b93358bb780ecbbdc8a1b2c7f5e97e1bcfb30e
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-08-07 14:43:39 +0200

    Update _slug.vue

--- pages/actualites/_slug.vue
+++ pages/actualites/_slug.vue
@@ -14,8 +14,6 @@
       <div class="sidebar">
         <ui-categories/>
         <ui-articles/>
-        <div id="fb-root"></div>
-        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v4.0&appId=170190880284997"></script>
         <div class="fb-page" data-href="https://www.facebook.com/residence1plus2/" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/residence1plus2/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/residence1plus2/">Résidence 1+2</a></blockquote></div>
       </div>
   </main>
