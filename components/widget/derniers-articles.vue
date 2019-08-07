73d2cfc65fb8c9a94d5739940d4c4d44d2ed7303
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-08-07 13:53:33 +0200

    Create derniers-articles.vue

---
+++ components/widget/derniers-articles.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="sidebar">
+      <div class="accordeon">
+          <div class="accordeon__head">
+              <div class="accordeon__titre">
+                  <h4>CATÉGORIES</h4>
+              </div>
+              <div class="accordeon__icon">
+                  <i class="fas fa-angle-down collapsible__icon"></i>
+              </div>
+          </div>
+          <div class="accordeon__body">
+              <div class="accordeon__content">
+                  <div class="accordeon__info" v-for="post in posts">
+                     <nuxt-link class="horaires" :to="post._path+'/'">{{ post.title }}</nuxt-link>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+    },
+    data() {
+      const context = require.context('~/content/categorie/page/', false, /\.json$/);
+      const posts = context.keys().map(key => ({
+        ...context(key),
+        _path: `/categories/${key.replace('.json', '').replace('./', '')}`
+      }));
+      return { posts };
+    },
+    mounted() {
+      document.querySelectorAll('.collapsible').forEach(el => el.addEventListener('click', e => {
+        e.currentTarget.classList.toggle('collapsible--open')
+      }));
+    }
+  };
+</script>
