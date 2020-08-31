44906dcd6ba74a14c1ccecd31abdc297835e78b4
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2020-08-31 10:58:34 +0200

    Create index.vue

---
+++ pages/partenaires/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="page-index">
+    PARTENAIRES
+  </main>
+</template>
+<script>
+  import $ from 'jquery'
+  export default {
+    layout: 'default',
+    transition: { name: 'intro', mode: 'out-in' },
+    components: { },
+    mounted() {
+      $("body").removeClass('red-page yellow-page blue-page');
+      this.titre();
+  },
+  destroyed() {
+  },
+  methods: {
+      titre(){
+          var modif = 'ACCUEIL';
+          $('.page-title').html( modif );           
+      }
+  }
+}
+</script>
+
