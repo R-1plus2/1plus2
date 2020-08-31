bb3c18f4cf17c960faaa8bae0e2b74747ffe8dd1
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2020-08-31 14:15:55 +0200

    Update index.vue

--- pages/partenaires/index.vue
+++ pages/partenaires/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <main class="page-index">
+  <main class="page-partenaires">
     PARTENAIRES
+    <slider/>
   </main>
 </template>
 <script>
   import $ from 'jquery'
+  // Ui
+  // Widget
+  import slider from '~/components/widget/slider.vue'
   export default {
     layout: 'default',
     transition: { name: 'intro', mode: 'out-in' },
@@ -13,12 +17,21 @@
       $("body").removeClass('red-page yellow-page blue-page');
       this.titre();
   },
+  components: {
+    slider
+  },
+  destroyed() {
+  },
+  mounted() {
+    $("body").removeClass('red-page yellow-page blue-page');
+    this.titre();
+  },
   destroyed() {
   },
   methods: {
       titre(){
           var modif = 'ACCUEIL';
-          $('.page-title').html( modif );           
+          $('.page-title').html( modif );
       }
   }
 }
