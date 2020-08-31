8f27a981ca2b4e58be5a5ad8583725a77a36d855
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2020-08-31 14:19:54 +0200

    Create slider.vue

---
+++ components/widget/slider.vue
@@ -0,0 +1,11 @@
+<template>
+  <no-ssr>
+  <carousel :autoplay="true" :nav="false">
+    <img src="https://www.piks-l.com/img/5.jpg">
+    <img src="https://www.piks-l.com/img/4.jpg">
+    <img src="https://www.piks-l.com/img/3.jpg">
+    <img src="https://www.piks-l.com/img/2.jpg">
+    <img src="https://www.piks-l.com/img/1.jpg">
+  </carousel>
+</no-ssr>
+</template>
