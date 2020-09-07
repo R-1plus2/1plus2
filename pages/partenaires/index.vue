329970b7a72b205241215e4d0a2bdb6c2c318c6e
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2020-09-07 09:35:34 +0200

    Update index.vue

--- pages/partenaires/index.vue
+++ pages/partenaires/index.vue
@@ -1,43 +1,43 @@
 <template>
   <main class="page-partenaires">
     <div class="left-side">
-        <div class="diapo">
+        <div class="grid">
             <h3>PARTENAIRES OFFICIELS</h3>
             <div v-for="i in officiels.images" class="image">
               <a :href="i.link"><img class="selected" :src="i.image" :alt="i.alt"></a>
             </div>
         </div>
-        <div class="diapo">
+        <div class="grid">
             <h3>PARTENAIRES INSTITUTIONNELS ET PRIVÉS</h3>
             <div v-for="i in institutionnels.images" class="image">
               <a :href="i.link"><img class="selected" :src="i.image" :alt="i.alt"></a>
             </div>
         </div>
-        <div class="diapo">
+        <div class="grid">
             <h3>PARTENAIRES SCIENTIFIQUES</h3>
             <div v-for="i in scientifiques.images" class="image">
               <a :href="i.link"><img class="selected" :src="i.image" :alt="i.alt"></a>
             </div>
         </div>
-        <div class="diapo">
+        <div class="grid">
             <h3>LIEUX ASSOCIÉS</h3>
             <div v-for="i in lieuxasso.images" class="image">
               <a :href="i.link"><img class="selected" :src="i.image" :alt="i.alt"></a>
             </div>
         </div>
-        <div class="diapo">
+        <div class="grid">
             <h3>PROGRAMMES ASSOCIÉS</h3>
             <div v-for="i in prograsso.images" class="image">
               <a :href="i.link"><img class="selected" :src="i.image" :alt="i.alt"></a>
             </div>
         </div>
-        <div class="diapo">
+        <div class="grid">
             <h3>PROGRAMMES ASSOCIÉS</h3>
             <div v-for="i in prograsso.images" class="image">
               <a :href="i.link"><img class="selected" :src="i.image" :alt="i.alt"></a>
             </div>
         </div>
-        <div class="diapo">
+        <div class="grid">
             <h3>PARTENAIRES MÉDIAS</h3>
             <div v-for="i in medias.images" class="image">
               <a :href="i.link"><img class="selected" :src="i.image" :alt="i.alt"></a>
