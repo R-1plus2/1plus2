027bf04ef3709c2b9b4c279d8f126d7feded3f23
Zymeth <Zymeth@users.noreply.github.com>
2020-09-07 13:30:23 +0200

    Update index.vue

--- pages/colloques/index.vue
+++ pages/colloques/index.vue
@@ -56,6 +56,7 @@ export default {
     },
   mounted() {
       $("body").removeClass('red-page yellow-page blue-page');
+      $("body").addClass('red-page');
       this.titre();
       this.ea();
   },
@@ -77,7 +78,6 @@ export default {
           sortBy : 'date',
           sortAscending : true
         });
-        $(grid.filteredItems[0].element).addClass('big');
         setTimeout(function(){grid.layout(); }, 100);
         
       }
