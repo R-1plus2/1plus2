8321c713bcb325f152906412aa408fa1fe19851d
Yoann PIKS-L <50371128+PuxianAlHazred@users.noreply.github.com>
2020-09-08 15:22:51 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,54 @@
 <template>
   <main class="page-index">
-  
-      <article v-for="i in fact" class="small-article facto-home">
-          <a class="article-padding factory-lien" :href="i.facto">
-              <img class="cover" :src="i.photoo"/>
-              <div class="content factory-image">
-                  <h3 class="title-article factory-nom">{{ i.facto}}</h3>
-              </div>
-          </a>
-      </article>
-      
+    <div class="colx4">
+        <h2>FACTORY</h2>
+        <article v-for="i in fact" class="small-article facto-home">
+            <a class="article-padding factory-lien" :href="i.facto">
+                <img class="cover" :src="i.photoo"/>
+                <div class="content factory-image">
+                    <h3 class="title-article factory-nom">{{ i.facto}}</h3>
+                </div>
+            </a>
+        </article>
+        <a class="read-more" href="/factory/">FACTORY</a>
+    </div>
+    <div class="colx4">
+        <h2>RESIDENCES</h2>
+        <article v-for="i in resi" class="small-article facto-home">
+            <a class="article-padding factory-lien" :href="i.resid">
+                <img class="cover" :src="i.photoo"/>
+                <div class="content factory-image">
+                    <h3 class="title-article factory-nom">{{ i.resid}}</h3>
+                </div>
+            </a>
+        </article>
+        <a class="read-more" href="/residences/">RESIDENCES</a>
+    </div> 
+    <div class="colx4">
+        <h2>FACTORY</h2>
+        <article v-for="i in fact" class="small-article facto-home">
+            <a class="article-padding factory-lien" :href="i.facto">
+                <img class="cover" :src="i.photoo"/>
+                <div class="content factory-image">
+                    <h3 class="title-article factory-nom">{{ i.facto}}</h3>
+                </div>
+            </a>
+        </article>
+        <a class="read-more" href="/factory/">FACTORY</a>
+    </div> 
+    <div class="colx4">
+        <h2>FACTORY</h2>
+        <article v-for="i in fact" class="small-article facto-home">
+            <a class="article-padding factory-lien" :href="i.facto">
+                <img class="cover" :src="i.photoo"/>
+                <div class="content factory-image">
+                    <h3 class="title-article factory-nom">{{ i.facto}}</h3>
+                </div>
+            </a>
+        </article>
+        <a class="read-more" href="/factory/">FACTORY</a>
+    </div> 
+    
   </main>
 </template>
 <script>
