ab26472d0f1631380946c4b24b6eaf33f03c9c81
Yoann PIKS-L <50371128+PuxianAlHazred@users.noreply.github.com>
2020-09-10 13:56:21 +0200

    Update footer.vue

--- components/ui/footer.vue
+++ components/ui/footer.vue
@@ -4,6 +4,7 @@
         <a target="_blank" href="https://fr-fr.facebook.com/residence1plus2/"><i class="fab fa-facebook"></i></a>
         <a target="_blank" href="https://twitter.com/residence1plus2"><i class="fab fa-twitter"></i></a>
         <a target="_blank" href="https://www.instagram.com/residence1plus2/"><i class="fab fa-instagram"></i></a>
+        <a target="_blank" href="https://fr.linkedin.com/company/residence-1-2"><i class="fab fa-linkedin"></i></a>
     </div>
     <div class="copyright"> 
       <p>WEBSITE BY <a target="_blank" href="https://www.piks-l.fr/">PIKS-L.FR</a> & <a target="_blank" href="https://www.iamsailor.com/">IAMSAILOR</a></p>     
