83c370093f83ebad8bd831d5ca4aa956a797d28f
R-1plus2 <70942043+R-1plus2@users.noreply.github.com>
2020-09-18 13:53:30 +0200

    Update menu.vue

--- components/ui/menu.vue
+++ components/ui/menu.vue
@@ -14,7 +14,8 @@
             <li><nuxt-link to="/colloques/" class="color-red menu-link">COLLOQUES</nuxt-link></li>  
             <li><nuxt-link to="/studies/" class="menu-link">STUDIES</nuxt-link></li>
             <li><nuxt-link to="/partenaires/" class="menu-link">PARTENAIRES</nuxt-link></li>
-            <li><nuxt-link to="/contacts/" class="menu-link">INFOS - CONTACTS</nuxt-link></li>
+            <li><nuxt-link to="/infos/" class="menu-link">INFOS</nuxt-link></li>
+            <li><nuxt-link to="/contacts/" class="menu-link">CONTACTS</nuxt-link></li>
           </ul>
       </nav>
   </div>
