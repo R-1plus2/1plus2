d3487d3c81000505411628f807225255d8a07258
rbiamsailor <50863502+rbiamsailor@users.noreply.github.com>
2020-09-21 11:58:55 +0200

    Update menu.vue

--- components/ui/menu.vue
+++ components/ui/menu.vue
@@ -3,18 +3,18 @@
       <div class="black-line"><div class="close">FERMER</div></div>
       <nav>
           <ul class="nav">
-            <li><nuxt-link to="/actualites/" class="menu-link">ACTUALITES</nuxt-link></li>
-            <li><nuxt-link to="/residences/" class="color-red menu-link">RESIDENCES</nuxt-link></li>
+            <li><nuxt-link to="/actualites/" class="menu-link">ACTUALITÉS</nuxt-link></li>
+            <li><nuxt-link to="/residences/" class="color-red menu-link">RÉSIDENCES</nuxt-link></li>
             <li><nuxt-link to="/factory/" class="color-blue menu-link">FACTORY</nuxt-link></li>
             <li><nuxt-link to="/hors-les-murs/" class="color-yellow menu-link">HORS LES MURS</nuxt-link></li>
             <li><nuxt-link to="/photographes/" class="menu-link">PHOTOGRAPHES</nuxt-link></li>
             <li><nuxt-link to="/expositions/" class="menu-link">EXPOSITIONS</nuxt-link></li>
-            <li><nuxt-link to="/editions/" class="color-red menu-link">EDITIONS</nuxt-link></li>
+            <li><nuxt-link to="/editions/" class="color-red menu-link">ÉDITIONS</nuxt-link></li>
             <li><nuxt-link to="/films/" class="color-red menu-link">FILMS</nuxt-link></li>
             <li><nuxt-link to="/colloques/" class="color-red menu-link">COLLOQUES</nuxt-link></li>  
             <li><nuxt-link to="/studies/" class="menu-link">STUDIES</nuxt-link></li>
             <li><nuxt-link to="/partenaires/" class="menu-link">PARTENAIRES</nuxt-link></li>
-            <li><nuxt-link to="/infos/" class="menu-link">INFOS</nuxt-link></li>
+            <li><nuxt-link to="/infos/" class="menu-link">À PROPOS</nuxt-link></li>
             <li><nuxt-link to="/contacts/" class="menu-link">CONTACTS</nuxt-link></li>
           </ul>
       </nav>
